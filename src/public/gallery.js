909a51907f0ef92867d944a99aa139c6b8542aff
Darrin Whitley <whitledt@gmail.com>
2022-02-25 13:33:09 -0500

    Finally able to get the rover printed on the screen

--- src/public/gallery.js
+++ src/public/gallery.js
@@ -9,68 +9,50 @@ let store = {
 }
 
 const root = document.getElementById('root')
-// const curiosity = document.getElementById('Curiosity')
 
-const renderCameraList = (cameraList, store) => {
-    console.log(store)
-    cameraList.innerHTML = store.selectedRover.name
-    render(root, store)
+const renderCameraList = async (store) => {
+    const cameraOptions = document.getElementById('cameraOptions')
+    cameraOptions.innerHTML = `<p>${store.selectedRover.rover}</p>`
 }
 
-const displayCameras = (roverName) => {
-    const cameraList = document.getElementById('cameraList')
-    getCameras(roverName)
-        .then(() => {
-            renderCameraList(cameraList, store)
-        })
+const renderRoverOptions = async (store) => {
+    const roverOptions = document.getElementById('roverOptions')
+    roverOptions.innerHTML = RoverOptions(store)
+
+    const curiosity = document.getElementById('Curiosity')
+    curiosity.addEventListener('click', () => {
+        getCameras('Curiosity')
+    })
+
+    const opportunity = document.getElementById('Opportunity')
+    opportunity.addEventListener('click', () => {
+        getCameras('Opportunity')
+    })
+
+    const spirit = document.getElementById('Spirit')
+    spirit.addEventListener('click', () => {
+        getCameras('Spirit')
+    })
 }
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store)
-        .then(() => {
-            const curiosity = document.getElementById('Curiosity')
-            curiosity.addEventListener('click', () => {
-                displayCameras('Curiosity')
-            })
-        })
+    renderRoverOptions(store)
 })
 
 const render = async (root, state) => {
     root.innerHTML = App(state)
 }
 
-const App = (state) => {
-    let { rovers, selectedRover } = state
-
+const  RoverOptions = (state) => {
+    let { rovers } = state
     return `
-        <header></header>
-        <main>
-            <section>
-                <h3>Select a rover!</h3>
-                ${Rovers(rovers)}
-                ${Cameras(selectedRover)}
-                <p id="cameraList"></p>
-                <p>Here is an example section.</p>
-            </section>
-        </main>
-        <footer></footer>
-    `
-}
-
-const clickRover = (rover) => {
-    var cameraList = document.getElementById('cameras')
-    console.log("hey hey")
-    // getCameras(store)
-
-    // cameraList.innerHTML = Cameras
-}
-
-const Rovers = (rovers) => {
-    return `
-        <button type="button" id="${rovers[0]}">${rovers[0]}</button>
-        <button type="button" id="${rovers[1]}">${rovers[1]}</button>
-        <button type="button" id="${rovers[2]}">${rovers[2]}</button>
+        <section>
+            <h3>Select a rover!</h3>
+            <button type="button" id="${rovers[0]}">${rovers[0]}</button>
+            <button type="button" id="${rovers[1]}">${rovers[1]}</button>
+            <button type="button" id="${rovers[2]}">${rovers[2]}</button>
+        </section>
     `
 }
 
@@ -80,24 +62,13 @@ const Cameras = (rover) => {
     `
 }
 
-// const Cameras = (rovers) => {
-//     return `
-//         <p>${getCameras(rovers[0])}</p>
-//     `
-// }
-
-const updateStore = (oldStore, newState) => {
+const updateStore = async (oldStore, newState) => {
     store = Object.assign(oldStore, newState)
-    // render(root, store)
-    // console.log(store)
+    renderCameraList(store)
 }
 
 const getCameras = async (roverName) => {
-    // let { selectedRover } = state
-
     fetch(`http://localhost:3000/${roverName}/cameras`)
         .then(res => res.json())
         .then(selectedRover => updateStore(store, { selectedRover }))
-
-    // return data
 }
